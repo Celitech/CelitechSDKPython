f768d07e4cde8393fcd22229d2c1d152fcd52636
zgheibali <80042395+zgheibali@users.noreply.github.com>
2024-04-22 17:56:35 +0300

    liblab SDK update for version v1.1.51 (#26)

--- install.cmd
+++ install.cmd
@@ -15,7 +15,7 @@ if "%USE_VENV%"=="1" (
 
 pip install build
 python -m build --outdir dist .
-pip install dist\celitech_sdk-1.1.50-py3-none-any.whl --force-reinstall
+pip install dist\celitech_sdk-1.1.51-py3-none-any.whl --force-reinstall
 
 if "%USE_VENV%"=="1" (
     deactivate
