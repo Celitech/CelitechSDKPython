741939eb21e19f4b4f9608d8a051656bf149b379
zgheibali <80042395+zgheibali@users.noreply.github.com>
2024-07-23 17:36:36 +0300

    liblab SDK update for version v1.1.71 (#45)

--- examples/install.cmd
+++ examples/install.cmd
@@ -2,4 +2,4 @@ python -m venv .venv
 call .venv\Scripts\activate
 pip install build
 python -m build --outdir dist ..\
-pip install dist\celitech_sdk-1.1.68-py3-none-any.whl --force-reinstall
+pip install dist\celitech_sdk-1.1.71-py3-none-any.whl --force-reinstall
