8f5d546cbb3a173d5caa74d9deb24885c1a787fe
Celitech <80042395+zgheibali@users.noreply.github.com>
2024-11-05 15:48:32 +0000

    liblab SDK update for version v1.2.4

--- examples/install.sh
+++ examples/install.sh
@@ -2,4 +2,4 @@ python -m venv .venv
 . .venv/bin/activate
 pip install build
 python -m build --outdir dist ../
-pip install dist/celitech_sdk-1.2.1-py3-none-any.whl --force-reinstall
+pip install dist/celitech_sdk-1.2.4-py3-none-any.whl --force-reinstall
