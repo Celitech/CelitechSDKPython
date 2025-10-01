b21ad871c3530d78c9c4a2f379f2994745e67e7a
zgheibali <80042395+zgheibali@users.noreply.github.com>
2025-10-01 19:15:26 +0300

    liblab SDK update for version v1.3.55 (#171)

--- src/celitech/models/get_esim_ok_response.py
+++ src/celitech/models/get_esim_ok_response.py
@@ -4,7 +4,11 @@ from .utils.sentinel import SENTINEL
 
 
 @JsonMap(
-    {"smdp_address": "smdpAddress", "manual_activation_code": "manualActivationCode"}
+    {
+        "smdp_address": "smdpAddress",
+        "manual_activation_code": "manualActivationCode",
+        "is_top_up_allowed": "isTopUpAllowed",
+    }
 )
 class GetEsimOkResponseEsim(BaseModel):
     """GetEsimOkResponseEsim
@@ -17,6 +21,8 @@ class GetEsimOkResponseEsim(BaseModel):
     :type manual_activation_code: str, optional
     :param status: Status of the eSIM, possible values are 'RELEASED', 'DOWNLOADED', 'INSTALLED', 'ENABLED', 'DELETED', or 'ERROR', defaults to None
     :type status: str, optional
+    :param is_top_up_allowed: Indicates whether the eSIM is currently eligible for a top-up. This flag should be checked before attempting a top-up request., defaults to None
+    :type is_top_up_allowed: bool, optional
     """
 
     def __init__(
@@ -25,6 +31,7 @@ class GetEsimOkResponseEsim(BaseModel):
         smdp_address: str = SENTINEL,
         manual_activation_code: str = SENTINEL,
         status: str = SENTINEL,
+        is_top_up_allowed: bool = SENTINEL,
         **kwargs
     ):
         """GetEsimOkResponseEsim
@@ -37,6 +44,8 @@ class GetEsimOkResponseEsim(BaseModel):
         :type manual_activation_code: str, optional
         :param status: Status of the eSIM, possible values are 'RELEASED', 'DOWNLOADED', 'INSTALLED', 'ENABLED', 'DELETED', or 'ERROR', defaults to None
         :type status: str, optional
+        :param is_top_up_allowed: Indicates whether the eSIM is currently eligible for a top-up. This flag should be checked before attempting a top-up request., defaults to None
+        :type is_top_up_allowed: bool, optional
         """
         self.iccid = self._define_str(
             "iccid", iccid, nullable=True, min_length=18, max_length=22
@@ -48,6 +57,7 @@ class GetEsimOkResponseEsim(BaseModel):
             "manual_activation_code", manual_activation_code, nullable=True
         )
         self.status = self._define_str("status", status, nullable=True)
+        self.is_top_up_allowed = is_top_up_allowed
         self._kwargs = kwargs
 
 
