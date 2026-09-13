2d26473073072608b337e66425f73b5a946ddd2e
zgheibali <80042395+zgheibali@users.noreply.github.com>
2026-09-13 15:21:19 -0400

    SDK update for version v2.0.7 (#219)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- src/celitech/net/transport/serializer.py
+++ src/celitech/net/transport/serializer.py
@@ -45,7 +45,7 @@ class Serializer:
         self.errors: dict[int, ApiError] = {}
         self.config: Optional["SdkConfig"] = config
 
-        self.headers["User-Agent"] = "postman-codegen/2.6.0 celitech-sdk/2.0.6 (python)"
+        self.headers["User-Agent"] = "postman-codegen/2.6.0 celitech-sdk/2.0.7 (python)"
 
         # Apply default headers
         for header in default_headers:
